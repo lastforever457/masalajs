90867fe63ef8b752713580c8fb215556ee7c9a93
unknown <lastforever457@gmail.com>
2024-07-11 13:57:32 +0500

    Third changes

--- src/Functions/Comment.tsx
+++ src/Functions/Comment.tsx
@@ -1,4 +1,9 @@
-function Comment(props) {
+interface IComment {
+    userName: string;
+    comment: string;
+}
+
+function Comment(props: IComment) {
     return (
         <li className="list-group-item">
             <h4>${props.userName}</h4>
